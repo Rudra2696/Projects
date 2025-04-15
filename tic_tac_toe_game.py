8fd79e4bb2e910936d93fb16b5c66ab04fd4749a
Rudra Patel <rhpatel1023@gmail.com>
2025-04-15 21:11:09 +0530

    rearrange code

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,5 +1,15 @@
 print("Welcome to tic tac toe game")
 
+try:
+    a=input("What is your choice : ")
+except(ValueError,SyntaxError,TypeError):
+        print("Something went wrong!!")               
+
+try:
+    b=input("Enter name of 2nd player : ")
+except(ValueError,SyntaxError,TypeError):
+        print("Something went wrong!!")
+
 print(f"For O Enter 1\n For X Enter 2")
 
 print("""
@@ -14,15 +24,6 @@ print("""
 """)
 
         
-try:
-    a=input("What is your choice : ")
-except(ValueError,SyntaxError,TypeError):
-        print("Something went wrong!!")               
-
-try:
-    b=input("Enter name of 2nd player : ")
-except(ValueError,SyntaxError,TypeError):
-        print("Something went wrong!!")
 
 while True:
     try:
