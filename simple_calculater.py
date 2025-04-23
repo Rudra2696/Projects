a13ff609a30e520a246a07954d88bcd39117fb2a
Rudra Patel <rhpatel1023@gmail.com>
2025-04-23 13:19:05 +0530

    added remainder

--- simple_calculater.py
+++ simple_calculater.py
@@ -7,6 +7,10 @@ def sub(a,b):
 def mul(a,b):
     return a*b  
 
+def mod(a,b):
+    return a%b
+
+
 def div(a,b):
     return a/b
 
@@ -16,8 +20,8 @@ while True:
     print("2.Subtract")
     print("3.Multiply")
     print("4.Divide")
-    choice = input("Enter choice(1/2/3/4): ")
-    if choice in ('1', '2', '3', '4'):
+    choice = input("Enter choice(1/2/3/4/5): ")
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         if choice == '1':
@@ -28,6 +32,8 @@ while True:
             print(num1, "*", num2, "=", mul(num1, num2))
         elif choice == '4':
             print(num1, "/", num2, "=", div(num1, num2))
+        elif choice == '5':
+            print(num1, "%", num2, "=", mod(num1, num2))
         break
     else:
-        print("Please choose 1/2/3/4 only") 
+        print("Please choose 1/2/3/4/5 only") 
